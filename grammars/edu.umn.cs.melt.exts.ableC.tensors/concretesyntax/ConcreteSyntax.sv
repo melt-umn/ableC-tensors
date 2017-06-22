498291ad0dc5587b7d94884cb9b7ebc555dcc342
Zoe <wentz101.umn>
2017-06-22 15:38:15 -0500

    gonna work on concrete syntax -zoe

--- grammars/edu.umn.cs.melt.exts.ableC.tensors/concretesyntax/ConcreteSyntax.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.tensors/concretesyntax/ConcreteSyntax.sv
@@ -8,18 +8,37 @@ imports edu:umn:cs:melt:exts:ableC:tensors:abstractsyntax;
 
 imports silver:langutil only ast;
 
-marking terminal ArrayEnvOpen_t '[.';
-terminal ArrayEnvClose_t '.]';
+
+marking terminal TensorEnvOpen_t '[.';
+terminal TensorEnvClose_t '.]';
 
 marking terminal leftParen '(';
 terminal rightParen ')';
 
 marking terminal tensorPrint 'print';
-
 marking terminal freeDynamic 'free_dynamic';
-
 marking terminal free 'free';
 
-concrete production free_dynamic
-e::Expr ::= 'free' '(' Tensor ')'
+concrete production single_element_tensor_c
+e::PrimaryExpr_c ::= '[.' value :: AssignExpr_c '.]'
+{
+  e.ast = float_to_scalar_tensor (value.ast, location = left.location);
+}
 
+concrete production print_tensor_c
+e::PrimaryExpr_c ::= 'print' '(' value :: AssignExpr_c ')'
+{
+  e.ast = print_tensor(value.ast,location = left.location);
+}
+
+concrete production free_tensor_c
+e::PrimaryExpr_c ::= 'free' '(' value :: AssignExpr_c ')'
+{
+  e.ast = free_tensor(value.ast,location = left.location);
+}
+
+concrete production free_tensor_dynamic_c
+e::PrimaryExpr_c ::= 'free_dynamic' '(' value :: AssignExpr_c ')'
+{
+  e.ast = free_tensor_dynamic(value.ast,location = left.location);
+}
