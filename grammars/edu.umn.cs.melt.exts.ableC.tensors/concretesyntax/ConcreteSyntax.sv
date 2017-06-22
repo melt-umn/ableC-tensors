70ee2fd487ed1247bae5aacf6906be80646d637a
Ankit <ankit.siva@gatech.edu>
2017-06-22 11:10:03 -0500

    ConcreteSyntax created

---
+++ grammars/edu.umn.cs.melt.exts.ableC.tensors/concretesyntax/ConcreteSyntax.sv
@@ -0,0 +1,23 @@
+grammar edu:umn:cs:melt:exts:ableC:tensors:concretesyntax;
+
+imports edu:umn:cs:melt:ableC:concretesyntax;
+imports edu:umn:cs:melt:ableC:abstractsyntax;
+imports edu:umn:cs:melt:ableC:abstractsyntax:construction;
+
+imports edu:umn:cs:melt:exts:ableC:tensors:abstractsyntax;
+
+imports silver:langutil only ast;
+
+marking terminal ArrayEnvOpen_t '[.';
+terminal ArrayEnvClose_t '.]';
+
+marking terminal leftParen '(';
+terminal rightParen ')';
+
+marking terminal tensorPrint 'print';
+
+marking terminal freeDynamic 'free_dynamic';
+
+marking terminal free 'free';
+
+
