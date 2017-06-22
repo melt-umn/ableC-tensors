a380c6a12feeb06454362052a6b1aecc814884e3
Zoe <wentz101.umn>
2017-06-22 16:18:46 -0500

    almost done with the ones that odn't need pointers (not including location???) -zoe

--- grammars/edu.umn.cs.melt.exts.ableC.tensors/concretesyntax/ConcreteSyntax.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.tensors/concretesyntax/ConcreteSyntax.sv
@@ -9,7 +9,6 @@ imports edu:umn:cs:melt:exts:ableC:tensors:abstractsyntax;
 
 imports silver:langutil only ast;
 
-
 --lol idk how to reference pointers? so avoiding all the equations that use pointers lol
 --also not sure how location works and where it's needed, so i'm not using it anywhere yet
 
@@ -22,7 +21,7 @@ terminal TensorEnvClose_t '.]';
 
 marking terminal create_tensor 'create';
 marking terminal access_tensor 'access';
-marking terminal copy_tensor 'copy';
+marking terminal copy_tensor 'copy'; --done
 marking terminal transpose 'trans'; --done
 marking terminal identity_tensor 'id'; --done
 marking terminal identity_tensor_asymmetric 'id_as';
@@ -32,18 +31,24 @@ marking terminal zeros 'zeros';
 marking terminal tensor_to_float 'ten_to_float'; --done
 
 marking terminal map 'map';
-marking terminal square_all 'square';
-marking terminal increment 'inc';
+marking terminal square_all 'square'; --done
+marking terminal increment 'inc'; --done
 
 marking terminal fold 'fold';
-marking terminal max 'max';
-marking terminal min 'min';
-marking terminal sum 'sum';
-marking terminal product 'prod';
-marking terminal tensor_max 'ten_max';
-marking terminal tensor_min 'ten_min';
-marking terminal tensor_sum 'ten_sum';
-marking terminal tensor_product 'ten_prod';
+marking terminal max 'max'; --done
+marking terminal min 'min'; --done
+marking terminal sum 'sum'; --done
+marking terminal product 'prod'; --done
+marking terminal tensor_max 'ten_max'; --done
+marking terminal tensor_min 'ten_min'; --done
+marking terminal tensor_sum 'ten_sum'; --done
+marking terminal tensor_product 'ten_prod'; --done
+
+marking terminal tensor_combine 'tensor_combine';
+marking terminal tensor_elem_add 'tensor_elem_add';
+marking terminal tensor_elem_subtract 'tensor_elem_subtract';
+marking terminal tensor_elem_multiply 'tensor_elem_multiply';
+marking terminal tensor_elem_divide 'tensor_elem_divide';
 
 marking terminal tensor_print 'print'; --done
 marking terminal free_dynamic 'free_dynamic'; --done
@@ -57,6 +62,12 @@ e::PrimaryExpr_c ::= '[.' value :: FloatConstant_t '.]'
   e.ast = float_to_scalar_tensor_a (value.ast, location = left.location);
 }
 
+concrete production copy_tensor_c
+e::PrimaryExpr_c ::= 'copy' '(' value :: AssignExpr_c ')';
+{
+  e.ast = copy_tensor_a(value.ast);
+}
+
 concrete production transpose_tensor_c
 e::PrimaryExpr_c ::= 'trans' '(' value :: AssignExpr_c ')';
 {
@@ -64,7 +75,7 @@ e::PrimaryExpr_c ::= 'trans' '(' value :: AssignExpr_c ')';
 }
 
 concrete production identity_tensor_c
-e::PrimaryExpr_c ::= 'id' '(' numDim :: DecConstant_t sizeDim :: DecConstant_t ')'
+e::PrimaryExpr_c ::= 'id' '(' numDim :: DecConstant_t ',' sizeDim :: DecConstant_t ')'
 {
   e.ast = identity_tensor_a(numDim.ast, sizeDim.ast);
 }
@@ -73,6 +84,90 @@ concrete production scalar_tensor_to_float
 e::PrimaryExpr_c ::= 'ten_to_float' '(' value :: AssignExpr_c ')'
 {
   e.ast = scalar_tensor_to_float_a(value.ast);
+}
+
+concrete production square_c
+e::PrimaryExpr_c ::= 'square' '(' value :: AssignExpr_c ')'
+{
+  e.ast = square_a(value.ast);
+}
+
+concrete production increment_c
+e::PrimaryExpr_c ::= 'inc' '(' value :: AssignExpr_c ')'
+{
+  e.ast = increment_a(value.ast);
+}
+
+concrete production max_c
+e::PrimaryExpr_c ::= 'max' '(' value :: AssignExpr_c ')'
+{
+  e.ast = max_a(value.ast);
+}
+
+concrete production min_c
+e::PrimaryExpr_c ::= 'min' '(' value :: AssignExpr_c ')'
+{
+  e.ast = min_a(value.ast);
+}
+
+concrete production sum_c
+e::PrimaryExpr_c ::= 'sum' '(' value :: AssignExpr_c ')'
+{
+  e.ast = sum_a(value.ast);
+}
+
+concrete production product_c
+e::PrimaryExpr_c ::= 'prod' '(' value :: AssignExpr_c ')'
+{
+  e.ast = product_a(value.ast);
+}
+
+concrete production tensor_max_c
+e::PrimaryExpr_c ::= 'ten_max' '(' value :: AssignExpr_c ')'
+{
+  e.ast = tensor_max_a(value.ast);
+}
+
+concrete production tensor_min_c
+e::PrimaryExpr_c ::= 'ten_min' '(' value :: AssignExpr_c ')'
+{
+  e.ast = tensor_min_a(value.ast);
+}
+
+concrete production tensor_sum_c
+e::PrimaryExpr_c ::= 'ten_sum' '(' value :: AssignExpr_c ')'
+{
+  e.ast = tensor_sum_a(value.ast);
+}
+
+concrete production tensor_product_c
+e::PrimaryExpr_c ::= 'ten_prod' '(' value :: AssignExpr_c ')'
+{
+  e.ast = tensor_product_a(value.ast);
+}
+
+concrete production tensor_elem_add_c
+e::PrimaryExpr_c ::= 'tensor_elem_add' '(' valueOne :: AssignExpr_c ',' valueTwo :: AssignExpr_c ')'
+{
+  e.ast = tensor_elem_add_a(valueOne.ast,valueTwo.ast);
+}
+
+concrete production tensor_elem_subtract_c
+e::PrimaryExpr_c ::= 'tensor_elem_subtract' '(' valueOne :: AssignExpr_c ',' valueTwo :: AssignExpr_c ')'
+{
+  e.ast = tensor_elem_subtract_a(valueOne.ast,valueTwo.ast);
+}
+
+concrete production tensor_elem_multiply_c
+e::PrimaryExpr_c ::= 'tensor_elem_multiply' '(' valueOne :: AssignExpr_c ',' valueTwo :: AssignExpr_c ')'
+{
+  e.ast = tensor_elem_multiply_a(valueOne.ast,valueTwo.ast);
+}
+
+concrete production tensor_elem_divide_c
+e::PrimaryExpr_c ::= 'tensor_elem_divide' '(' valueOne :: AssignExpr_c ',' valueTwo :: AssignExpr_c ')'
+{
+  e.ast = tensor_elem_divide_a(valueOne.ast,valueTwo.ast);
 }
 
 concrete production print_tensor_c
