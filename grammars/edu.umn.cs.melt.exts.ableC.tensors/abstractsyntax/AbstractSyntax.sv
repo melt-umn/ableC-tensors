130bcaf3181e03bc354a066525250c914383270d
Zoe Wentzel <wentz101@umn.edu>
2017-06-28 16:37:35 -0500

    this is broken, trying to write the float_to_tensor function -zoe

--- grammars/edu.umn.cs.melt.exts.ableC.tensors/abstractsyntax/AbstractSyntax.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.tensors/abstractsyntax/AbstractSyntax.sv
@@ -7,6 +7,10 @@ imports silver:langutil:pp;
 imports silver:langutil;
 imports edu:umn:cs:melt:ableC:abstractsyntax:env;
 
+--i changed it to just float_to_scalar_tensor_a to begin with since that's the one most similar to matlab
+--when i try to make, it can't find the function for some reason. i'm not sure why, but it makes me sad. :(
+
+{-
 abstract production ones_a
 e::Expr ::= numDim :: Expr sizeDim :: Expr
 {
@@ -14,11 +18,30 @@ e::Expr ::= numDim :: Expr sizeDim :: Expr
   numDim.env = e.env;
   sizeDim.env = e.env;
 }
+-}
 
+abstract production float_to_scalar_tensor_a
+e::Expr ::= float :: Expr l = Location{
+	forwards to floatToScalarTensor(float, l);
+}
+
+{-
 abstract production print_tensor_a
 e::Expr ::= tensor :: Expr{
   forwards to directCallExpr (
     name(ones), tensor
   );
   tensor.env = e.env;
+}
+-}
+
+function floatToScalarTensor
+Expr ::= float :: Expr loc = Location
+{
+	return 
+		directCallExpr(
+			name("float_to_scalar_tensor", location = l),
+			consExpr (float, nilExpr()),
+			location = l
+		);
 }
