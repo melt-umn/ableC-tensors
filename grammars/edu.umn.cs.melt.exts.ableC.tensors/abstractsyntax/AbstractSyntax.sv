ce8a53cf762a71a7b585d4cbf484d6f76dd716ac
Ankit <ankit.siva@gatech.edu>
2017-06-29 16:56:48 -0500

    .gitignore changed to ignore the generated files and minor changes to AbstractSyntax

--- grammars/edu.umn.cs.melt.exts.ableC.tensors/abstractsyntax/AbstractSyntax.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.tensors/abstractsyntax/AbstractSyntax.sv
@@ -10,21 +10,25 @@ imports edu:umn:cs:melt:ableC:abstractsyntax:env;
 --i changed it to just float_to_scalar_tensor_a to begin with since that's the one most similar to matlab
 --when i try to make, it can't find the function for some reason. i'm not sure why, but it makes me sad. :(
 
-
 abstract production ones_a
 e::Expr ::= numDim :: Expr sizeDim :: Expr
 {
-  forwards to txtExpr("ones(" ++ show(80, numDim.pp) ++ ", " ++ show(80, sizeDim.pp) ++ ")", location = txtLoc("ableC-tensors"));
+  forwards to directCallExpr(
+    name(
+    "ones", 
+    location = txtLoc("ableC-tensors")),
+    consExpr (
+      numDim, 
+      consExpr(
+        sizeDim,
+        nilExpr()
+      )
+    ),
+    location = txtLoc("ableC-tensors")
+  );
+
   numDim.env = e.env;
   sizeDim.env = e.env;
-}
-
-
---check type of float in here probably? if it isn't a float we want to raise an error instead of passing it to the function
-abstract production float_to_scalar_tensor_a
-e::Expr ::= float :: Expr
-{
-	forwards to floatToScalarTensor(float, e.location);
 }
 
 abstract production print_tensor_a
@@ -34,15 +38,22 @@ e::Expr ::= tensor :: Expr
   tensor.env = e.env;
 }
 
+--check type of float in here probably? if it isn't a float we want to raise an error instead of passing it to the function
+abstract production float_to_scalar_tensor_a
+e::Expr ::= float :: Expr
+{
+  forwards to floatToScalarTensor(float, e.location);
+}
+
 --matlab splits up the c calls as functions, seperate from the abstract productions
 --do we wish to do this as well? :) 
 function floatToScalarTensor
 Expr ::= float :: Expr l :: Location
 {
-	return 
-		directCallExpr(
-			name("float_to_scalar_tensor", location = l),
-			consExpr (float, nilExpr()),
-			location = l
-		);
+  return 
+    directCallExpr(
+      name("float_to_scalar_tensor", location = l),
+      consExpr (float, nilExpr()),
+      location = l
+    );
 }
