dafb428cffed955dd9b20cc94852383161eb2131
Zoe <wentz101.umn>
2017-07-06 11:41:58 -0500

    going to work on a branch for a bit -zoe

--- grammars/edu.umn.cs.melt.exts.ableC.tensors/abstractsyntax/AbstractSyntax.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.tensors/abstractsyntax/AbstractSyntax.sv
@@ -724,25 +724,4 @@ e::Expr ::= tensor :: Expr
   );
 }
 
-<<<<<<< HEAD
---check type of float in here probably? if it isn't a float we want to raise an error instead of passing it to the function
-abstract production float_to_scalar_tensor_a
-e::Expr ::= float :: Expr
-{
-  forwards to floatToScalarTensor(float, e.location);
-}
-
---matlab splits up the c calls as functions, separate from the abstract productions
---do we wish to do this as well? :)
-function floatToScalarTensor
-Expr ::= float :: Expr l :: Location
-{
-  return
-    directCallExpr(
-      name("float_to_scalar_tensor", location = l),
-      consExpr (float, nilExpr()),
-      location = l
-    );
-}
-
 global loc::Location = builtinLoc("ableC-tensors");
