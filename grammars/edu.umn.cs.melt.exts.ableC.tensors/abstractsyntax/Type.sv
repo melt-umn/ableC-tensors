ef9fc80adbfc72db2232745fb186f5a6e78e1f94
Zoe <wentz101.umn>
2017-07-07 14:35:33 -0500

    how do i do this

--- grammars/edu.umn.cs.melt.exts.ableC.tensors/abstractsyntax/Type.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.tensors/abstractsyntax/Type.sv
@@ -11,6 +11,8 @@ abstract production tensorType
 top::Type ::= q::Qualifiers
 {
   --top.module = "edu:umn:cs:melt:exts:ableC:tensors:tensors"
+  top.lpp = pp"interval";
+  top.rpp = pp"";
   forwards to
   tagType(
     q,
