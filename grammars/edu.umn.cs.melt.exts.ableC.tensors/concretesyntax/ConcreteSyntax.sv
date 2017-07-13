55eb4dd9032bde1153590b82e6b827cd617d8047
Zoe <wentz101.umn>
2017-07-13 18:17:55 -0500

    capitalized terminal

--- grammars/edu.umn.cs.melt.exts.ableC.tensors/concretesyntax/ConcreteSyntax.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.tensors/concretesyntax/ConcreteSyntax.sv
@@ -9,8 +9,8 @@ imports edu:umn:cs:melt:exts:ableC:tensors:abstractsyntax;
 imports silver:langutil:pp;
 imports silver:langutil;
 
-marking terminal intervalEnvOpen_t '<.';
-terminal intervalEnvClose_t '.>';
+marking terminal IntervalEnvOpen_t '<.';
+terminal IntervalEnvClose_t '.>';
 
 marking terminal TensorEnvOpen_t '[.';
 terminal TensorEnvClose_t '.]';
