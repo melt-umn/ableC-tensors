1e97c16306439711a41df42b9bb6cccbbf23847d
Zoe <wentz101.umn>
2017-07-13 18:44:12 -0500

    where is the error coming from

--- grammars/edu.umn.cs.melt.exts.ableC.tensors/concretesyntax/ConcreteSyntax.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.tensors/concretesyntax/ConcreteSyntax.sv
@@ -100,15 +100,15 @@ e::PrimaryExpr_c ::= '[.' value :: AssignExpr_c '.]'
   e.ast = float_to_scalar_tensor_a(value.ast, location = e.location);
 }
 
-{-
+
 concrete productions top::PrimaryExpr_c
 | '<.' oneDim :: AssignExpr_c '.>'
   { top.ast = create_interval_double_bound_a(oneDim.ast, oneDim.ast,
     location = top.location); }
-| '<.' leftDim :: AssignExpr_c '-' rightDim :: AssignExpr_c '.>'
+| '<.' leftDim :: AssignExpr_c '-' '-'' rightDim :: AssignExpr_c '.>'
   { top.ast = create_interval_double_bound_a(leftDim.ast, rightDim.ast,
     location = top.location); }
-| '<.' leftDim :: AssignExpr_c '-' '*' '.>'
+| '<.' leftDim :: AssignExpr_c '-' '-' '*' '.>'
   { top.ast = create_interval_left_bound_a(leftDim.ast,
     location = top.location); }
 | '<.' '*' '.>'
@@ -116,9 +116,9 @@ concrete productions top::PrimaryExpr_c
 
 --rest of these are technically useless but will add for consistency
 
-| '<.' '*' '-' rightDim :: AssignExpr_c '.>'
+| '<.' '*' '-' '-' rightDim :: AssignExpr_c '.>'
   { top.ast = create_interval_right_bound_a(rightDim.ast, location = top.location); }
-| '<.' '*' '-' '*' '.>'
+| '<.' '*' '-' '-' '*' '.>'
   { top.ast = create_interval_no_bound_a(location = top.location); }
 -}
 
